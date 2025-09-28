e45bfeb5adbdb04bcdcd0edc5c17e55933eba797
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-09-28 16:56:56 +0700

    MODULE 4: Maven Management -The Parent POM -The dependencyManagement Element and BOM -Build Plugins -Repositories -Quick Peek at Additional Maven Features

---
+++ maven/learn-maven-module4/maven-dependency-management-end/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Campaign(id, code, name, description) VALUES (default, 'C1', 'Campaign 1', 'Description of Campaign 1');
+INSERT INTO Campaign(id, code, name, description) VALUES (default, 'C2', 'Campaign 2', 'About Campaign 2');
+INSERT INTO Campaign(id, code, name, description) VALUES (default, 'C3', 'Campaign 3', 'About Campaign 3');
