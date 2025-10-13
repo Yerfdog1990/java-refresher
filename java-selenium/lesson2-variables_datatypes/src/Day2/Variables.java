e22b3aeea2eff7fcde383f92de5fe07677d814d9
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-13 20:52:39 +0700

    Lesson 2: Variables & Data types -Java Variables and Data Types -Declaring Variables in Java -Primitive Data Types (int, float, double, char, boolean) -Variable Naming Conventions

---
+++ java-selenium/lesson2-variables_datatypes/src/Day2/Variables.java
@@ -0,0 +1,19 @@
+package Day2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        int c = 300;
+
+        System.out.println("The value of a is: " + a);
+        System.out.println("The value of b is: " + b);
+        System.out.println("The value of c is: " + c);
+
+        // Multiple variable declaration -> Only works if all values belong to the same data type.
+        int x = 10, y = 20, z = 30;
+        System.out.println("The value of x is: " + x);
+        System.out.println("The value of y is: " + y);
+        System.out.println("The value of z is: " + z);
+    }
+}
