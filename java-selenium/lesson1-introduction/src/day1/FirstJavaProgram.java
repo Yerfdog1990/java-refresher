e83da9ec4c6754ed98e0e566933492de472ab4b9
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-13 20:24:53 +0700

    Lesson 1: Introduction -Java and Eclipse Environment Setup -Setting up Java Development Kit (JDK) -Installing Eclipse IDE -Configuring Java and Eclipse Environment -How to Write Basic Java Program -Creating a Java Class -Writing the First Java Program -Compiling and Running Java Programs -Understanding Console Output.

---
+++ java-selenium/lesson1-introduction/src/day1/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+        System.out.println("10 + 20 = " +(10 + 20));
+        args = new String[]{"Hello", "World"};
+        System.out.println("First string element: " +args[0]);
+        System.out.println("Second string element: " +args[1]);
+    }
+}
