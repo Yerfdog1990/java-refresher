0af7ace71e619a4053f00ef30961d1d2bd272966
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-13 20:52:39 +0700

    Lesson 2: Variables & Data types -Java Variables and Data Types -Declaring Variables in Java -Primitive Data Types (int, float, double, char, boolean) -Variable Naming Conventions

---
+++ java-selenium/lesson2-variables_datatypes/src/Day2/DataTypes.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class DataTypes {
+    static void main(String[] args) {
+        // Primitive data types -> Can only store a single data at a time.
+        byte b = 127;           // byte type
+        short s = 32767;        // short type
+        int age = 30;           // integer type
+        long i = 123456789L;    // long data type -> declared with literal (L)
+        float price = 10.25F;   // decimal number -> maximum 7 decimal digits -> declared with literal (F)
+        double d = 3.14159;     // decimal number -> maximum 15 decimal digits
+        char grade = 'A';       // single character
+        boolean isPassed = true;// boolean type
+
+    }
+}
