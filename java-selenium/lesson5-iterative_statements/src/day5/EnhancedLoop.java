f14fcc518165135e74b6195ed8b13dff47500418
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-16 20:30:01 +0700

    Added enhanced loop.

---
+++ java-selenium/lesson5-iterative_statements/src/day5/EnhancedLoop.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class EnhancedLoop {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+    }
+}
