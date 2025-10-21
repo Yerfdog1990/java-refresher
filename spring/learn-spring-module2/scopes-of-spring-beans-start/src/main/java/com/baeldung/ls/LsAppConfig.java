7ab1996ff1e5095b2b115a844f6a126b8181f6f5
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-22 06:05:53 +0700

    Module 1 - Getting Started With Spring -Lesson 1: Why Spring? -Lesson 2: Understanding the Full Spring Ecosystem -Lesson 3: Introducing Spring Boot -Lesson 4: A Simple Development Environment -Lesson 5: The Real-World Project We’re Building -Lesson 6: Maven and Spring

--- spring/learn-spring-module2/scopes-of-spring-beans-start/src/main/java/com/baeldung/ls/LsAppConfig.java
+++ spring/learn-spring-module2/scopes-of-spring-beans-start/src/main/java/com/baeldung/ls/LsAppConfig.java
@@ -1,8 +1,21 @@
 package com.baeldung.ls;
 
+import com.baeldung.ls.persistence.repository.IProjectRepository;
+import com.baeldung.ls.persistence.repository.impl.ProjectRepositoryImpl;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class LsAppConfig {
+    @Bean
+    //@Scope("singleton") -> Spring creates a single instance of a bean, and all requests for such a bean will return the same object, which will be cached.
+    //@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 
+    //@Scope("prototype") -> Spring will create a new instance every time a bean is requested
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public IProjectRepository singletonBean() {
+        return new ProjectRepositoryImpl();
+    }
 }
