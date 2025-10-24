e991e5e5061a61526d801c93bc1eb5ce89cbf841
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-24 16:30:04 +0700

    Spring boot testing

---
+++ spring/learn-spring-module3/m3-spring-testing-framework-lesson-start/src/test/java/com/baeldung/ls/service/ProjectServiceIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.baeldung.ls.service;
+
+import com.baeldung.ls.persistance.config.TestConfig;
+import com.baeldung.ls.persistence.model.Project;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringJUnitConfig(classes = {TestConfig.class})
+public class ProjectServiceIntegrationTest {
+
+    @Autowired
+    private IProjectService projectService;
+
+    @Test
+    public void whenSavingProject_thenOK() {
+        Project savedProject = projectService.save(new Project("name", LocalDate.now()));
+        assertThat(savedProject, is(notNullValue()));
+    }
+
+}
