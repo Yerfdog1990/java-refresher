462da6a0f8a64d8d9d8f65edd8163b8723a6c9ba
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-24 16:30:04 +0700

    Spring boot testing

---
+++ spring/learn-spring-module3/m3-spring-testing-framework-lesson-start/src/test/java/com/baeldung/ls/persistance/config/TestConfig.java
@@ -0,0 +1,9 @@
+package com.baeldung.ls.persistance.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.baeldung.ls")
+public class TestConfig {
+}
