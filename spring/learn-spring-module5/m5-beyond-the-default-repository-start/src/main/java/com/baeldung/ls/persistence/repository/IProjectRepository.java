0aafa3489bc1d71be14f82a0d208e7703a9ed9b7
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-28 05:29:28 +0700

    Lesson 3: Pagination and sorting -Understand how pagination and sorting work in Spring Data JPA. -Use the Pageable, PageRequest, Sort, and Page<T> classes effectively. -Implement RESTful endpoints that return paginated and sorted results. -Customize sorting behavior using Sort objects.

--- spring/learn-spring-module5/m5-beyond-the-default-repository-start/src/main/java/com/baeldung/ls/persistence/repository/IProjectRepository.java
+++ spring/learn-spring-module5/m5-beyond-the-default-repository-start/src/main/java/com/baeldung/ls/persistence/repository/IProjectRepository.java
@@ -4,5 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.baeldung.ls.persistence.model.Project;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface IProjectRepository extends CrudRepository<Project, Long> {
+    Optional<Project> findByName(String name);
+
+    List<Project> findByDateCreatedBetween(LocalDate start, LocalDate end);
 }
--- spring/learn-spring-module5/m5-pagination-and-sorting-start/src/test/java/com/baeldung/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
+++ spring/learn-spring-module5/m5-pagination-and-sorting-start/src/test/java/com/baeldung/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
@@ -1,7 +1,20 @@
 package com.baeldung.ls.persistence.repository;
 
+import com.baeldung.ls.persistence.model.Project;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ProjectRepositoryIntegrationTest {
@@ -9,4 +22,29 @@ public class ProjectRepositoryIntegrationTest {
     @Autowired
     IProjectRepository projectRepository;
 
+    @Test
+    public void givenDataCreated_whenFindAllPaginated_thenSuccess() {
+        Page<Project> retrievedProjects = projectRepository.findAll(PageRequest.of(0, 10));
+        assertThat(retrievedProjects.getContent(), hasSize(6));
+    }
+
+    @Test
+    public void givenDataCreated_whenFindAllAllSorted_thenSuccess() {
+        Sort ascendingSort = Sort.by(Sort.Order.asc("name"));
+        List<Project> retrievedProjects = projectRepository.findAll(ascendingSort);
+
+        List<Project> sortedProjects = new ArrayList<>(retrievedProjects);
+
+        sortedProjects.sort(Comparator.comparing(Project::getName));
+        assertEquals(sortedProjects,  retrievedProjects);
+    }
+
+    @Test
+    public void givenDataCreated_whenFindAllAndSorted_thenSuccess() {
+        Sort ascendingSort = Sort.by(Sort.Order.asc("name"));
+        PageRequest pageRequest = PageRequest.of(0, 10, ascendingSort);
+        List<Project> retrievedProjects = projectRepository.findAll(pageRequest).getContent();
+
+        assertThat(retrievedProjects, hasSize(6));
+    }
 }
