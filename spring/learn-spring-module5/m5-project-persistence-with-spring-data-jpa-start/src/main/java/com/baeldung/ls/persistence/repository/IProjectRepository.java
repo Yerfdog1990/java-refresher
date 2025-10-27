7b601884ce86d316edffcd69d93271d22888b7f3
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-10-27 20:26:50 +0700

    Lesson 1: Persistance with Spring data -Understand what Spring Data JPA is and why it’s used. -Differentiate between CrudRepository and JpaRepository. -Implement a basic JPA entity and repository interface. -Perform CRUD operations with Spring Boot. -Explain why no-args constructors are required in JPA entities.

--- spring/learn-spring-module5/m5-project-persistence-with-spring-data-jpa-start/src/main/java/com/baeldung/ls/persistence/repository/IProjectRepository.java
+++ spring/learn-spring-module5/m5-project-persistence-with-spring-data-jpa-start/src/main/java/com/baeldung/ls/persistence/repository/IProjectRepository.java
@@ -1,12 +1,9 @@
 package com.baeldung.ls.persistence.repository;
 
-import java.util.Optional;
-
 import com.baeldung.ls.persistence.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface IProjectRepository {
+public interface IProjectRepository extends JpaRepository<Project, Long> {
 
-    Optional<Project> findById(Long id);
-
-    Project save(Project project);
 }
