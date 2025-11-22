8137b9ec580460eb29d101e3fd7218bcad6ac94f
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-11-22 12:30:50 +0700

    Name queries in spring data.

--- spring-data/learn-spring-data-module3/named-queries-start/src/main/java/com/baeldung/lsd/NamedQueriesApp.java
+++ spring-data/learn-spring-data-module3/named-queries-start/src/main/java/com/baeldung/lsd/NamedQueriesApp.java
@@ -1,5 +1,7 @@
 package com.baeldung.lsd;
 
+import com.baeldung.lsd.persistence.model.Campaign;
+import jakarta.persistence.EntityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.baeldung.lsd.persistence.repository.CampaignRepository;
 
+import java.util.List;
+
 @SpringBootApplication
 public class NamedQueriesApp implements ApplicationRunner {
 
@@ -18,13 +22,45 @@ public class NamedQueriesApp implements ApplicationRunner {
     @Autowired
     private CampaignRepository campaignRepository;
 
+    @Autowired
+    EntityManager entityManager;
+
     public static void main(final String... args) {
         SpringApplication.run(NamedQueriesApp.class, args);
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // Invoke the Named Query with an EntityManager instance
+        List<Campaign> campaignsWithIdGt1 = entityManager.createNamedQuery("namedQueryCampaignsWithIdGreaterThan", Campaign.class)
+                .setParameter("id", 1L)
+                .getResultList();
+        LOG.info("Find Campaigns with Id greater than 1 using EntityManager:\n{}", campaignsWithIdGt1);
 
+        // Calling Named Query referenced in Spring Data JPA Repositories
+        List<Campaign> campaignsWithIdLt3 = campaignRepository
+                .findCampaignsWithIdLessThan(3L);
+        LOG.info("Find Campaigns with Id less than 3:\n{}", campaignsWithIdLt3);
+
+        // Modifying Named Queries
+        campaignRepository.updateCampaignDescriptionById(1L,
+                "New description updated by named query");
+
+        Campaign campaign1 = campaignRepository.findById(1L)
+                .get();
+
+        LOG.info("After updating the description of the Campaign(id=1):\n{}",
+                campaign1);
+
+        List<Campaign> campaignsWithShortDescription = campaignRepository
+                .findCampaignsWithDescriptionShorterThan(16);
+        LOG.info("Find Campaigns with description shorter than 16:\n{}",
+                campaignsWithShortDescription);
+
+        List<Campaign> campaignsWithDescriptionPrefix = campaignRepository
+                .findCampaignsWithDescriptionPrefix("About");
+        LOG.info( "Find Campaigns with Description Prefix (NamedQuery from properties file):\n{}",
+                campaignsWithDescriptionPrefix);
     }
 
 }
