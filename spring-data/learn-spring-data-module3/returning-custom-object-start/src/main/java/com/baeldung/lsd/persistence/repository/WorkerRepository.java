fb772aff8c6475988f28265bd116eeeebfaf3c6f
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-11-22 13:38:42 +0700

    Spring Data JPA Projections

--- spring-data/learn-spring-data-module3/returning-custom-object-start/src/main/java/com/baeldung/lsd/persistence/repository/WorkerRepository.java
+++ spring-data/learn-spring-data-module3/returning-custom-object-start/src/main/java/com/baeldung/lsd/persistence/repository/WorkerRepository.java
@@ -1,9 +1,13 @@
 package com.baeldung.lsd.persistence.repository;
 
+import com.baeldung.lsd.persistence.projection.WorkerOpen;
 import org.springframework.data.repository.CrudRepository;
 
 import com.baeldung.lsd.persistence.model.Worker;
 
-public interface WorkerRepository extends CrudRepository<Worker, Long> {
+import java.util.List;
 
+public interface WorkerRepository extends CrudRepository<Worker, Long> {
+    // Open Projections
+    List<WorkerOpen> findByFirstName(String firstName);
 }
