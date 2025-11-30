1b9327c09e4a20c9052ed116e2975fef63ff6943
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-11-30 08:21:24 +0700

    Intro to Spring Security

---
+++ spring-security-core/learn-spring-security-lssc-module1/m1-lesson2/src/test/java/com/baeldung/test/Lss2IntegrationTest.java
@@ -0,0 +1,18 @@
+package com.baeldung.test;
+
+import com.baeldung.lss.spring.LssApp2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LssApp2.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+public class Lss2IntegrationTest {
+
+    @Test
+    public void whenLoadApplication_thenSuccess() {
+
+    }
+}
