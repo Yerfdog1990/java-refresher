b5da97c134fcd86ee519f74a47388815fee81352
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-03 05:20:35 +0700

    Spring Security Anonymous Authentication

--- spring-security-core/learn-spring-security-lssc-module1/m1-lesson6/src/main/java/com/baeldung/lss/spring/LssApp6.java
+++ spring-security-core/learn-spring-security-lssc-module1/m1-lesson6/src/main/java/com/baeldung/lss/spring/LssApp6.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 import com.baeldung.lss.persistence.InMemoryUserRepository;
 import com.baeldung.lss.persistence.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 @ComponentScan("com.baeldung.lss.web")
@@ -19,6 +21,10 @@ public class LssApp6 {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(new Class[] { LssApp6.class, LssSecurityConfig.class, LssWebMvcConfiguration.class }, args);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(auth);
+
     }
 
 }
