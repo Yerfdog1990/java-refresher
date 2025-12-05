a045c950bb13326f329e8a54ae350cfa861d1106
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-05 11:59:45 +0700

    Basic spring security authetication

--- spring-security-core/learn-spring-security-lssc-module2/m2-lesson3/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
--- Test User
-insert into User (id, email, password, created) values (1, 'test@email.com', 'pass', '2008-08-08 00:00:00');
---
+++ spring-security-core/m1-spring-security-intro/l1-basic-authentication/src/test/java/springsecurity/l1basicauthentication/L1BasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package springsecurity.l1basicauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1BasicAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
