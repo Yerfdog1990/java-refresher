cf564732b72b03e778e776c2c78ffc52a61e41c3
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-06 19:21:10 +0700

    Spring Security URL Authorization

---
+++ spring-security-core/m1-introduction/url-authorization/src/main/java/springsecurity/urlauthorization/configuration/SecurityConfig.java
@@ -0,0 +1,52 @@
+package springsecurity.urlauthorization.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        UserDetails user =
+                User.builder()
+                        .username("user")
+                        .password(encoder.encode("password"))
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
---
+++ spring-security-core/m1-introduction/url-authorization/src/test/java/springsecurity/urlauthorization/UrlAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package springsecurity.urlauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
