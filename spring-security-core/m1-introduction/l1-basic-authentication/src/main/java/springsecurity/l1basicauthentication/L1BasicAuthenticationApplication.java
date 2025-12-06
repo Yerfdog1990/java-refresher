55791b7ae7d2bcb9150256c5cf041f9f74779490
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-06 08:19:09 +0700

    Spring security basic authentication

---
+++ spring-security-core/m1-introduction/l1-basic-authentication/src/main/java/springsecurity/l1basicauthentication/L1BasicAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package springsecurity.l1basicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1BasicAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1BasicAuthenticationApplication.class, args);
+    }
+
+}
