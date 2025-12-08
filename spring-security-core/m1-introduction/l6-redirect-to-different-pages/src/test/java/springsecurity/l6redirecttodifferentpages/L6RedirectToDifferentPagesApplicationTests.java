36dc17ee3e2ccc82b53f0d8f52a6ccab8d17470a
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-08 18:52:50 +0700

    Redirecting to Different Pages + Custom Logout

---
+++ spring-security-core/m1-introduction/l6-redirect-to-different-pages/src/test/java/springsecurity/l6redirecttodifferentpages/L6RedirectToDifferentPagesApplicationTests.java
@@ -0,0 +1,13 @@
+package springsecurity.l6redirecttodifferentpages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L6RedirectToDifferentPagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
