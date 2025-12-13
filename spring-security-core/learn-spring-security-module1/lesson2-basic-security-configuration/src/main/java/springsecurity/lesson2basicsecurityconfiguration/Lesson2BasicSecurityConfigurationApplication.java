44708fd0548cb42db8e617a46480b3465c12c53c
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-13 21:00:13 +0400

    Added spring security dependency and used auto generated password to sign in.

---
+++ spring-security-core/learn-spring-security-module1/lesson2-basic-security-configuration/src/main/java/springsecurity/lesson2basicsecurityconfiguration/Lesson2BasicSecurityConfigurationApplication.java
@@ -0,0 +1,13 @@
+package springsecurity.lesson2basicsecurityconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson2BasicSecurityConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson2BasicSecurityConfigurationApplication.class, args);
+    }
+
+}
