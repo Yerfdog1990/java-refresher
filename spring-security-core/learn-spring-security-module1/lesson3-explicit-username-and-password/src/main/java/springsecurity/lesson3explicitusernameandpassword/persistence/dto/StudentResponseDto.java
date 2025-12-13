4f6af7ae56d4481a6e66fddc667d9cec145a6119
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-13 21:22:29 +0400

    Configure spring security setting (username and password) explicitly in application.properties file.

---
+++ spring-security-core/learn-spring-security-module1/lesson3-explicit-username-and-password/src/main/java/springsecurity/lesson3explicitusernameandpassword/persistence/dto/StudentResponseDto.java
@@ -0,0 +1,19 @@
+package springsecurity.lesson3explicitusernameandpassword.persistence.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Calendar;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentResponseDto {
+    private Long id;
+    private String username;
+    private String email;
+    private Calendar dateCreated;
+}
---
+++ spring-security-core/learn-spring-security-module1/lesson3-explicit-username-and-password/src/test/java/springsecurity/lesson3explicitusernameandpassword/Lesson3ExplicitUsernameAndPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package springsecurity.lesson3explicitusernameandpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson3ExplicitUsernameAndPasswordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
