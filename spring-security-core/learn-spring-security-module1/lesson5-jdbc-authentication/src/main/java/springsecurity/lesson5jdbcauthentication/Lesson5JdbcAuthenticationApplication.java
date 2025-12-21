5f9b6fa2f391d472123551bdea60e03d67c9e917
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-21 13:40:41 +0300

    Jdbc user authentication.

---
+++ spring-security-core/learn-spring-security-module1/lesson5-jdbc-authentication/src/main/java/springsecurity/lesson5jdbcauthentication/Lesson5JdbcAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package springsecurity.lesson5jdbcauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson5JdbcAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson5JdbcAuthenticationApplication.class, args);
+    }
+
+}
---
+++ spring-security-core/learn-spring-security-module1/lesson5-jdbc-authentication/src/main/java/springsecurity/lesson5jdbcauthentication/validation/OnCreate.java
@@ -0,0 +1,6 @@
+package springsecurity.lesson5jdbcauthentication.validation;
+
+/**
+ * Marker interface for validation constraints that should apply on create only.
+ */
+public interface OnCreate {}
