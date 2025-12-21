8483ecee93648dc99c873dac7e61f607ac2add21
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-21 13:39:42 +0300

    In-memory user authentication

---
+++ spring-security-core/learn-spring-security-module1/lesson4-inmemory-authentication/src/main/java/springsecurity/lesson4inmemoryauthentication/Lesson4InmemoryAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package springsecurity.lesson4inmemoryauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson4InmemoryAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson4InmemoryAuthenticationApplication.class, args);
+    }
+
+}
---
+++ spring-security-core/learn-spring-security-module1/lesson4-inmemory-authentication/src/main/java/springsecurity/lesson4inmemoryauthentication/persistence/dto/StudentRequestDto.java
@@ -0,0 +1,27 @@
+package springsecurity.lesson4inmemoryauthentication.persistence.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import springsecurity.lesson4inmemoryauthentication.validation.OnCreate;
+
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"username", "email"})
+public class StudentRequestDto {
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required", groups = OnCreate.class)
+    private String password;
+}
