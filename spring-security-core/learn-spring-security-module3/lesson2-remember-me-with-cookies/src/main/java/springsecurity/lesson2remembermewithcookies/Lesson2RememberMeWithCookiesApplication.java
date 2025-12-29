46a5416f88ab934907e6ecbf9a675a41d9d7c91d
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-29 19:50:05 +0300

    Cookie-based remember me

---
+++ spring-security-core/learn-spring-security-module3/lesson2-remember-me-with-cookies/src/main/java/springsecurity/lesson2remembermewithcookies/Lesson2RememberMeWithCookiesApplication.java
@@ -0,0 +1,13 @@
+package springsecurity.lesson2remembermewithcookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson2RememberMeWithCookiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson2RememberMeWithCookiesApplication.class, args);
+    }
+
+}
