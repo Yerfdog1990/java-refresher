85632d73eee6cc95eac749d8d973c4dbf78a1af7
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-12-29 17:46:26 +0300

    Deleted cloned registrtaion flow diractories

--- learn-spring-security-lssc-module2/m2-lesson5/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
--- Test User
-insert into User (id, email, password, enabled, created) values (1, 'test@email.com', 'pass', true, '2008-08-08 00:00:00');
--- learn-spring-security-lssc-module2/m2-lesson6/src/main/java/com/baeldung/lss/persistence/SecurityQuestionDefinitionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.lss.persistence;
-
-import com.baeldung.lss.model.SecurityQuestionDefinition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SecurityQuestionDefinitionRepository extends JpaRepository<SecurityQuestionDefinition, Long> {
-
-}
