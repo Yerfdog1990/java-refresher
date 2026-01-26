28834d2c5b2895a11292e01c87086e4709ad1a7e
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2026-01-27 05:48:25 +0700

    Handling web tables

--- java-selenium/lesson32-handling-auto-suggest-dropdown-and-web-tables/src/test/java/day32/HandleWebTablesTest.java
+++ java-selenium/lesson32-handling-auto-suggest-dropdown-and-web-tables/src/test/java/day32/HandleWebTablesTest.java
@@ -90,4 +90,47 @@ public class HandleWebTablesTest {
             }
         }
     }
+
+    @Test
+    public void givenStaticTable_whenPrintBooksByAuthor_thenSuccess() {
+        // Rows
+        List<WebElement> rowList = driver.findElements(By.xpath("//table[@name='BookTable']//tr"));
+
+        for (int i = 2; i < rowList.size(); i++) {
+            String author = driver.findElement(By.xpath("//table[@name='BookTable']//tr["+i+"]//td[2]")).getText();
+            if(author.contains("Mukesh")){
+                String book = driver.findElement(By.xpath("//table[@name='BookTable']//tr["+i+"]//td[1]")).getText();
+                log.info("Author: {} -> Book: {}", author, book);
+                assertThat(book).containsAnyOf("Master In Selenium", "Learn Java");
+            }
+        }
+    }
+
+    @Test
+    public void givenStaticTable_whenPrintBooksByPrice_thenSuccess() {
+        // Rows
+        List<WebElement> rowList = driver.findElements(By.xpath("//table[@name='BookTable']//tr"));
+
+        int totalprice = 0;
+        for (int i = 2; i < rowList.size(); i++) {
+            // Retrieve prices
+            String price = driver.findElement(By.xpath("//table[@name='BookTable']//tr["+i+"]//td[4]")).getText();
+
+            // Update total price
+            totalprice += Integer.parseInt(price);
+
+            // Retrieve books
+            String book = driver.findElement(By.xpath("//table[@name='BookTable']//tr["+i+"]//td[1]")).getText();
+            log.info("Book: {} -> Price: {}", book, price);
+        }
+        // Print total price
+        log.info("Total price: {}", totalprice);
+    }
+
+    // TODO -> Assignment
+    /*
+    https://blazedemo.com/
+    Navigate the booking website to search for the cheapest flight to you destination then
+    receive confirmation message "Your flight from TLV to SFO has been reserved."
+     */
 }
